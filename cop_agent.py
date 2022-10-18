11a11e6c7a100f79243484cf018892f8c63bbec2
Adrian Durci <2403010d@student.gla.ac.uk>
2022-10-18 11:28:14 +0100

    Code skeleton structure implementation added

---
+++ cop_agent.py
@@ -0,0 +1,15 @@
+
+import random
+import interaction_model as model
+
+class CopBasic():
+    def __init__(self, graph: model.Graph, pos) -> None:
+        self.pos = pos
+        self.graph = graph
+    
+    def make_move(self):
+        # find best edge
+        options = self.graph.nodes[self.pos].adjacent
+
+        self.pos = random.choice(options)
+        
---
+++ interaction_model.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Node():    
+    def __init__(self, adjacent: List[Node]) -> None:
+        self.adjacent = adjacent
+        self.blocked = []
+
+class Graph():
+    def __init__(self, nodes: dict):
+        self.blocked_edges = {}
+        self.score = 0
+        self.cops = []
+        self.robbers = []
+        self.nodes = nodes
+    
+    def spawn_cop(self):
+        pass
+
+    def spawn_robber(self):
+        pass
+
+    def block_path(self, edge):
+        if edge in self.blocked_edges:
+            return False
+        else:
+            self.blocked_edges[edge] = -1
+            return True
+
+    def release_path(self, edge):
+        if edge not in self.blocked_edges:
+            return False
+        else:
+            del self.blocked_edges[edge]
+            return True
+
+    def eval_turn(self):
+        score += sum(self.blocked_edges.values())
+        for r in self.robbers:
+            for c in self.cops:
+                if r.pos == c.pos:
+                    return True # cop wins
+        return False
